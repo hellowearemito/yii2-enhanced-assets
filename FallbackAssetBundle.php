37e99025d8696368a4e08678f9bbb3f51d9e187b
Nikola Kovacs <n.kovacs@mito.hu>
2015-06-01 16:46:47 +0200

    use Json::htmlEncode to fix XSS

--- FallbackAssetBundle.php
+++ FallbackAssetBundle.php
@@ -61,7 +61,7 @@ class FallbackAssetBundle extends AssetBundle
                     "
                         (function() {
                             if (!{$this->check}) {
-                                var files = " . Json::encode($styles, 0) . ";
+                                var files = " . Json::htmlEncode($styles, 0) . ";
 
                                 for (var i = 0, l = files.length; i < l; i++) {
                                     var tag = document.createElement('link');
@@ -79,7 +79,7 @@ class FallbackAssetBundle extends AssetBundle
             }
 
             $view->jsFiles[$position][] = Html::script(
-                $this->check." || document.write(" . Json::encode($scripts, 0) . ");",
+                $this->check." || document.write(" . Json::htmlEncode($scripts, 0) . ");",
                 ['type' => 'text/javascript']
             );
         }
